fd896669e1979ba201e20c94262180f86bbfb52b
viniciusmeneses <vini23hot@gmail.com>
2018-12-05 19:44:33 -0200

    Installed styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+// import styled from 'styled-components';
+
+// Importando o estilo global criado
+import GlobalStyle from './styles/global';
+
+// Utilizando styled-components para criar um componente estlizado
+// O componente estilizado criado é acessível somente dentro deste componente
+
+// Sintaxe:
+// const Componente = styled.tagHtml`
+// css....
+// `;
+
+// Criando um h1 estilizado...
+// const Title = styled.h1`
+//   color: #f00;
+//   font-size: 32px;
+// `;
 
 const App = () => (
-  <div className="App">
-    <h1>Hello World!</h1>
-  </div>
+  <Fragment>
+    {/* Utilizando o estilo global importado para que funcione */}
+    <GlobalStyle />
+
+    <div className="App">
+      {/* Utilizando o styled-component criado */}
+      {/* <Title>Hello World!</Title> */}
+    </div>
+  </Fragment>
 );
 
 export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+// Para criar uma estilização global, devemos utilizar o crateGlobalStyle
+// Passando o CSS que queremos exportar
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #9b65E6;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialised !important;
+  }
+`;
+
+export default GlobalStyle;
